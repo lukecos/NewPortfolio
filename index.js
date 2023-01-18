2254dfb92cf9b9d25914f1559fcc8daf827b4323
lukecos <lukecose7483@gmail.com>
2023-01-18 11:14:39 +1100

    Added Head Start Homes page
    Added a new page via an extra .html file while utilizing the existing .CSS to contain the style.
    
    First pass - will be adding further imagery and design in future.

--- index.js
+++ index.js
@@ -62,3 +62,20 @@ $(".caseStudy-col3").on({
     $(this).css("border", "0px solid black");
   },
 });
+
+//----Head_Start_Homes-----//
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
